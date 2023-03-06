46ca346e22a58f17c8a96abbe2ebda290ab40554
梅宮翔太 <umemiyashouta@umemiyashoutanoMacBook-Pro.local>
2023-03-06 19:01:51 +0900

    authorization

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -7,11 +7,6 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $todoes = Todo::all();
@@ -19,11 +14,6 @@ class TodoController extends Controller
         return view('todoes.index', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         $todo = new Todo();
@@ -31,12 +21,6 @@ class TodoController extends Controller
         return view('todoes.create', $data);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -52,39 +36,22 @@ class TodoController extends Controller
         return redirect(route('todoes.index'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
     public function show(Todo $todo)
     {
         $data = ['todo' => $todo];
         return view('todoes.show', $data);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Todo $todo)
     {
+        $this->authorize($todo);
         $data = ['todo' => $todo];
         return view('todoes.edit', $data);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Todo $todo)
     {
+        $this->authorize($todo);
         $this->validate($request, [
             'title' => 'required|max:255',
             'body'=> 'required|max:255'
@@ -95,14 +62,9 @@ class TodoController extends Controller
         return redirect(route('todoes.show', $todo));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Todo $todo)
     {
+        $this->authorize($todo);
         $todo->delete();
         return redirect(route('todoes.index'));
     }
--- resources/views/todoes/show.blade.php
+++ resources/views/todoes/show.blade.php
@@ -4,6 +4,7 @@
     <h1 class="todo-title">{{ $todo->title }}</h1>
     <div class="todo-info">{{ $todo->created_at }}</div>
     <div class="todo-body">{!! nl2br(e($todo->body)) !!}</div>
+    @can('update', $todo)
     <div class="todo-control">
         <a href="{{ route('todoes.edit', $todo) }}">編集</a>
         <form onsubmit="return confirm('本当に削除しますか?')" action="{{ route('todoes.destroy', $todo) }}" method="post">
@@ -12,5 +13,6 @@
             <button type="submit">削除</button>
         </form>
     </div>
+    @endcan
 </todo>
 @endsection()
