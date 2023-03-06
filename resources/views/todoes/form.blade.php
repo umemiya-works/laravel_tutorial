6f90fa4219e1c3516a1855b9541096c58f2e0cc8
梅宮翔太 <umemiyashouta@umemiyashoutanoMacBook-Pro.local>
2023-03-06 12:27:26 +0900

    hold_title_and_body

--- resources/views/todoes/form.blade.php
+++ resources/views/todoes/form.blade.php
@@ -1,7 +1,7 @@
 @csrf
 <dl class="form-list">
     <dt>タイトル</dt>
-    <dd></dd><input type="text" name="title" value="{{ old('title') }}"></dd>
+    <dd><input type="text" name="title" value="{{ old('title', $todo->title) }}"></dd>
     <dt>本文</dt>
-    <dd><textarea name="body" rows="5">{{ old('body') }}</textarea></dd>
+    <dd><textarea name="body" rows="5">{{ old('body', $todo->body) }}</textarea></dd>
 </dl>
