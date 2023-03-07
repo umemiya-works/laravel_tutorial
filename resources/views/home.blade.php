2d18e2326aa50ff9d8bea690c825193981336f98
梅宮翔太 <umemiyashouta@umemiyashoutanoMacBook-Pro.local>
2023-03-07 10:49:21 +0900

    refactored_css_and_etc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 @section('content')
 @include('commons.errors')
-<div class="container">
-<h1>マイページ</h1>
+<h1 class="page-heading">マイページ</h1>
 <p>ようこそ、{{ Auth::user()->name }}さん | <a href="{{ route('todoes.create') }}">記事を書く</a></p>
 @include('todoes.todoes')
 @endsection()
