f27929a77163420c468bd12c01f6a00883fada53
umemiya_for_work <umemiya@mediowl.co.jp>
2023-03-20 14:43:23 +0900

    softdelete

---
+++ database/migrations/2023_03_20_143213_add_softdelete_to_tasks.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSoftdeleteToTasks extends Migration
+{
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+}
