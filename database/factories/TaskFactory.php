1864bf9b29d606fdcc3774292d6d57490b57fd72
umemiya_for_work <umemiya@mediowl.co.jp>
2023-03-22 17:20:28 +0900

    seed_data_for_tasks_and_users

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Task;
+use Faker\Generator as Faker;
+
+$factory->define(Task::class, function (Faker $faker) {
+    return [
+        'title' => $faker->title,
+        'body' => $faker->word,
+        'user_id' => function() {
+            return factory(App\Models\User::class)->create()->id;
+        }
+    ];
+});
---
+++ database/seeds/TaskTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TaskTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tasks = factory(App\Models\Task::class, 5)->create();
+    }
+}
