cb131ea7336ed9c2da2575118ace2547fa075349
umemiya_for_work <umemiya@mediowl.co.jp>
2023-03-28 10:49:21 +0900

    adjusted_flash_message

--- resources/views/commons/flash.blade.php
+++ resources/views/commons/flash.blade.php
@@ -1,9 +1,11 @@
-@if (session('success'))
-    <div class="flash alert alert-success">
-        {{ session('success') }}
-    </div>
-@elseif (session('danger'))
-    <div class="flash alert alert-danger">
-        {{ session('danger') }}
-    </div>
-@endif
+<div class="flash position-absolute top-10 z-3 w-100 justify-content-center">
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @elseif (session('danger'))
+        <div class="alert alert-danger">
+            {{ session('danger') }}
+        </div>
+    @endif
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="/public/css/app.css">
     <script src="https://kit.fontawesome.com/395b2a6823.js" crossorigin="anonymous"></script>
     <script src="{{ asset('/js/app.js') }}"></script>
 
@@ -15,7 +14,8 @@
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-light text-white">
         @include('commons.header')
     </header>
-    <main class="container">
+    <main class="container position-relative">
+        @include('commons.flash')
         @yield('content')
     </main>
     <footer class="footer mt-auto py-3 bg-light fixed-bottom">
